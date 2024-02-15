93921538270d95a7aa9d03cbef60b96783ef31eb
sami <kozlosam@gmail.com>
2024-02-15 16:13:26 +0530

    Lines on the model surface

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 import * as THREE from "three";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import fragmentMountain from "./shader/fragmentMountainTexture.glsl"
-import vertexMountain from "./shader/vertexMountainTexture.glsl"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import stripes from "../textures/stripes.png"
+import { MeshLine, MeshLineMaterial } from 'three.meshline';
 
 export class Sketch {
   constructor(options) {
@@ -48,10 +46,46 @@ export class Sketch {
       }
     };
 
-    this.addObjects();
     this.resize();
+    this.addObjects();
     this.render();
     this.setupResize();
+  }
+
+  populateIsolines() {
+    this.scene.updateMatrixWorld();
+    let lineCount = 20;
+    const modelXInterval = [-1, 1];
+    for (let i = 1; i < lineCount; i++) {
+      let tubeXPosition = modelXInterval[0] + i/lineCount*(modelXInterval[1]-modelXInterval[0]);
+      this.scene.add(this.getIsolineAt(tubeXPosition));
+    }
+  }
+
+  getIsolineAt(x) {
+    let probingYHeight = 10;
+    let probingCount = 100;
+    let probingDirection = new THREE.Vector3(0, -1, 0);
+    const modelZInterval = [ -1, 1 ];
+
+    let linePoints = [];
+    for (let i = 0; i < probingCount; i++) {
+      let zPosition = modelZInterval[0] + i/probingCount*(modelZInterval[1]-modelZInterval[0]);
+      let casterOrigin = new THREE.Vector3(x, probingYHeight, zPosition);
+      let caster = new THREE.Raycaster(casterOrigin, probingDirection);
+      let intersects = caster.intersectObjects(this.model.children);
+      if (intersects.length > 0) {
+        let intersection = intersects[0].point;
+        linePoints.push(new THREE.Vector3(intersection.x, intersection.y, intersection.z));
+      }
+    }
+
+    const geometry = new THREE.BufferGeometry().setFromPoints(linePoints);
+    const line = new MeshLine();
+    line.setGeometry(geometry);
+    let material = new MeshLineMaterial({ color: new THREE.Color(0xffffff), lineWidth: 0.002});
+    material.transparent = true;
+    return new THREE.Mesh(line, material);
   }
 
   setupResize() {
@@ -70,17 +104,13 @@ export class Sketch {
 
   addObjects() {
     const sketch = this;
-    this.material = this.getShaderMaterial();
     this.gltfLoader.load(
         '../textures/scene.gltf',
         function ( gltf ) {
           let loadedScene = gltf.scene;
-          loadedScene.traverse((o) => {
-            if (o.isMesh) {
-              o.material = sketch.material;
-            }
-          });
-          sketch.scene.add( loadedScene );
+          sketch.model = loadedScene;
+          sketch.model.updateMatrixWorld();
+          sketch.populateIsolines();
         },
         function ( xhr ) {
           console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
@@ -89,25 +119,6 @@ export class Sketch {
           console.log( 'An error happened:', error );
         }
     );
-  }
-
-  getShaderMaterial() {
-    let stripesTexture = new THREE.TextureLoader().load(stripes);
-    stripesTexture.wrapS = THREE.RepeatWrapping;
-    stripesTexture.wrapT = THREE.RepeatWrapping;
-
-    this.uniforms.uStripes = { value: stripesTexture };
-    return new THREE.ShaderMaterial({
-      extensions: {
-        derivatives: "#extension GL_OES_standard_derivatives : enable"
-      },
-      side: THREE.DoubleSide,
-      uniforms: this.uniforms,
-      transparent: true,
-      depthTest: false,
-      fragmentShader: fragmentMountain,
-      vertexShader: vertexMountain
-    });
   }
 
   stop() {
@@ -125,7 +136,6 @@ export class Sketch {
     if (!this.isPlaying) return;
     this.uniforms.uTime.value += 0.05;
 
-
     requestAnimationFrame(this.render.bind(this));
     this.renderer.render(this.scene, this.camera);
   }
@@ -141,6 +151,5 @@ document.onmousemove = function(e) {
 
   sketch.caster.setFromCamera(new THREE.Vector2(mousePositionX, mousePositionY), sketch.camera);
   let intersects = sketch.caster.intersectObjects(sketch.scene.children);
-
   sketch.uniforms.uMouseWorldPosition.value = intersects[0].point;
 }
