fa5e8fb0748ab7b85c36d19ca52f149ae5995c26
sami <kozlosam@gmail.com>
2024-02-15 13:39:26 +0530

    Texture distortion based on mouse position

--- js/shader/fragmentMountainTexture.glsl
+++ js/shader/fragmentMountainTexture.glsl
@@ -7,21 +7,14 @@ varying vec3 vWorldPosition;
 float PI = 3.141592653589793238;
 
 void main()	{
-    float time1 = uTime * 0.04;
+    float time = uTime * 0.04;
 
-    float dist = length(vWorldPosition - uMouseWorldPosition);
-    if (dist < 0.1) {
-        gl_FragColor = vec4(1);
-    } else {
+    vec3 offset = normalize(-vWorldPosition + uMouseWorldPosition)*(min(length(vWorldPosition - uMouseWorldPosition), 0.2) - 0.2)*vec3(1., sign(vWorldPosition.y - uMouseWorldPosition.y), 1.);
+    float texture1 = texture2D(uStripes, vUv + vec2(0., time) + offset.xy).r;
+    float texture11 = texture2D(uStripes, vUv + 1.5*(vec2(0., time) + offset.xy)).r;
 
-        float texture1 = texture2D(uStripes, vUv + vec2(time1, time1)).r;
-        float texture11 = texture2D(uStripes, vUv + vec2(time1 * 1.5)).r;
+    float alpha = min(texture1, texture11);
+    vec3 color = vec3(0.579, 0.903, 0.983);
 
-        float alpha = min(texture1, texture11);
-        vec3 color1 = vec3(0.579, 0.903, 0.983);
-
-        vec3 viewDir = -normalize(vWorldPosition.xyz - cameraPosition);
-
-        gl_FragColor = vec4(vec3(color1), alpha);
-    }
+    gl_FragColor = vec4(vec3(color), alpha);
 }
